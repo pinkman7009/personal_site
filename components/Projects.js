c555053c87e913dac654f68771e682948917efbd
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2021-04-14 16:05:08 +0530

    projects added without animation

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import { ProjectWrapper } from '../elements/ProjectElements';
+import {
+  ProjectWrapper,
+  ProjectsContent,
+  ProjectItem,
+} from '../elements/ProjectElements';
+import { H2 } from '../elements/TypographyElements';
+
 const Projects = () => {
-  return <ProjectWrapper id='projects'>These are my projects</ProjectWrapper>;
+  return (
+    <ProjectWrapper id='projects'>
+      <H2 size='large' textAlign='center' color='primary' smCenter='true'>
+        My Projects
+      </H2>
+      <ProjectsContent>
+        <ProjectItem bg='#D77F7F'>Bookify Online Book Library</ProjectItem>
+        <ProjectItem bg='#20B197'>Pet Store API</ProjectItem>
+        <ProjectItem bg='#DEDF98'>Movie Nominations</ProjectItem>
+        <ProjectItem bg='#CA9ECB'>Github Finder</ProjectItem>
+        <ProjectItem bg='#52A1EA'>WeatherLand Weather Search</ProjectItem>
+        <ProjectItem bg='#ADDBA6'>Animeaza Anime Blog</ProjectItem>
+      </ProjectsContent>
+    </ProjectWrapper>
+  );
 };
 
 export default Projects;
--- elements/ProjectElements.js
+++ elements/ProjectElements.js
@@ -2,7 +2,37 @@ import styled from 'styled-components';
 
 export const ProjectWrapper = styled.div`
   padding-top: 150px;
-  height: 100vh;
+  min-height: 100vh;
   width: 100%;
-  padding-left: 20px;
+  margin-top: 4rem;
+`;
+
+export const ProjectsContent = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+  height: 100%;
+  width: 100%;
+  padding: 2rem;
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+export const ProjectItem = styled.div`
+  min-height: 350px;
+  min-width: 200px;
+  background: ${(props) => props.bg};
+  padding: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+  cursor: pointer;
+  text-align: center;
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
 `;
